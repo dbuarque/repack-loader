9ff1ae46a358878c3f0e2029b67df46d05429d7e
dbuarque <diogenes.buarque@gmail.com>
2016-01-24 21:13:51 -0200

    test upgrade version npm

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,6 @@ const should = require('should');
 
 describe('RepackLoader', function() {
     it('should be and object', function () {
-        var obj = require('../tpl/repack.json');
         console.log(process.env.PWD)
     });
 
